c22313b41ccd9a92f02bab454b702c86fcb015ec
blublunv <blublunv@gmail.com>
2026-01-22 00:17:08 +0900

    chores: 컴포넌트 구조에 맞춰 생성중

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+import { fileURLToPath } from 'url' // 1. 이 줄 추가
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 2. __dirname을 요즘 방식(ESM)으로 직접 만들어주는 코드
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
