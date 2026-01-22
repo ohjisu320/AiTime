a23374081fa193352dabfa2cb31f2077f700046c
pyy2114 <pyy2114@naver.com>
2026-01-22 16:13:43 +0900

    [BE] chore: 예약 엔티티 관련 누락된 파일 추가

---
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/hospital/controller/ReservationController.java
@@ -0,0 +1,11 @@
+package com.ssafy.aitime.domain.hospital.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservation")
+@RequiredArgsConstructor
+public class ReservationController {
+}
---
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/hospital/service/ReservationService.java
@@ -0,0 +1,4 @@
+package com.ssafy.aitime.domain.hospital.service;
+
+public interface ReservationService {
+}
