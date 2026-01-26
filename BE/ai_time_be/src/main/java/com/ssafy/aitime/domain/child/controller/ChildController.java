16b367884263b34db6348e4afbbe27d1c436509d
박윤영 <pyy2114@naver.com>
2026-01-27 00:59:06 +0900

    [BE-147] feat: 아이 추가 기능 구현

--- BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/child/controller/ChildController.java
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/child/controller/ChildController.java
@@ -1,6 +1,17 @@
 package com.ssafy.aitime.domain.child.controller;
 
+import com.ssafy.aitime.common.response.ApiResponse;
+import com.ssafy.aitime.domain.child.dto.request.ChildCreateRequest;
+import com.ssafy.aitime.domain.child.dto.response.ChildCreateResponse;
+import com.ssafy.aitime.domain.child.service.ChildService;
+import com.ssafy.aitime.security.principal.UserPrincipal;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ChildController {
 
+    private final ChildService childService;
 
+    @PostMapping
+    public ResponseEntity<ApiResponse<ChildCreateResponse>> addChild(
+            @AuthenticationPrincipal UserPrincipal principal,
+            @Valid @RequestBody ChildCreateRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.created("아이 등록이 완료되었습니다.", childService.addChild(principal.getUserId(), request)));
+    }
 }
--- BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/child/service/ChildService.java
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/child/service/ChildService.java
@@ -1,4 +1,10 @@
 package com.ssafy.aitime.domain.child.service;
 
+import com.ssafy.aitime.domain.child.dto.request.ChildCreateRequest;
+import com.ssafy.aitime.domain.child.dto.response.ChildCreateResponse;
+
+import java.util.UUID;
+
 public interface ChildService {
+    ChildCreateResponse addChild(UUID userId, ChildCreateRequest request);
 }
