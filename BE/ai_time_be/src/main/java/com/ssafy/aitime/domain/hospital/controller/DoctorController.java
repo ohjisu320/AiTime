7a692d49831651fa98481f5d411dfc0f76f2c604
soojin <wls7911@naver.com>
2026-01-27 14:31:48 +0900

    [BE-239] test: 의사별 월별 예약 여부 조회 캘린더 테스트 코드 작성

--- BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/hospital/controller/DoctorController.java
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/hospital/controller/DoctorController.java
@@ -16,9 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-
-
 @RestController
 @RequestMapping("/doctor/")
 @RequiredArgsConstructor
