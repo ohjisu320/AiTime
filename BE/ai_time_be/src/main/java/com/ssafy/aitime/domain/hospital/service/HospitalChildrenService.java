497f94f352af8261b8487a9cab19844e6d4110c7
soojin <wls7911@naver.com>
2026-01-28 19:10:27 +0900

    [BE-305] feat: 연동 병원 목록 조회 API

--- BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/hospital/service/HospitalChildrenService.java
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/hospital/service/HospitalChildrenService.java
@@ -1,4 +1,10 @@
 package com.ssafy.aitime.domain.hospital.service;
 
+import com.ssafy.aitime.domain.hospital.dto.response.HospitalResponseDto;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface HospitalChildrenService {
+    List<HospitalResponseDto> getHospitalResponseDtosByChild(UUID childId);
 }
