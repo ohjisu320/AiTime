2c9fb3d520fba77ad7b12f178d8e7deb55054ce7
pyy2114 <pyy2114@naver.com>
2026-01-28 17:33:45 +0900

    [BE-185] feat: UserDetailService 분리

--- BE/ai_time_be/src/main/java/com/ssafy/aitime/security/service/CustomHospitalStaffDetailsService.java
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/security/service/CustomHospitalStaffDetailsService.java
@@ -1,8 +1,12 @@
 package com.ssafy.aitime.security.service;
 
 import com.ssafy.aitime.common.enums.RecordStatus;
+import com.ssafy.aitime.domain.hospital.entity.HospitalStaff;
+import com.ssafy.aitime.domain.hospital.repository.HospitalRepository;
+import com.ssafy.aitime.domain.hospital.repository.HospitalStaffRepository;
 import com.ssafy.aitime.domain.user.entity.User;
 import com.ssafy.aitime.domain.user.repository.UserRepository;
+import com.ssafy.aitime.security.principal.HospitalStaffPrincipal;
 import com.ssafy.aitime.security.principal.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,16 +16,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomHospitalStaffDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    private final HospitalStaffRepository hospitalStaffRepository;
 
     @Override
     public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
-        User user = userRepository
+        HospitalStaff staff = hospitalStaffRepository
                 .findByLoginIdAndRecordStatus(loginId, RecordStatus.ACTIVE)
                 .orElseThrow(() -> new UsernameNotFoundException(loginId));
 
-        return UserPrincipal.from(user);
+        return HospitalStaffPrincipal.from(staff);
     }
 }
