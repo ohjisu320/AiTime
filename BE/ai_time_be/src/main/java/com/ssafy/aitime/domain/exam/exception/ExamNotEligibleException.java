ae240865dd757c42c86b0bf09d1288df1487f1ee
soojin <wls7911@naver.com>
2026-01-30 15:48:19 +0900

    [BE-318] fix: ExamNotEligibleException 예외처리 수정

--- BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/exam/exception/ExamNotEligibleException.java
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/exam/exception/ExamNotEligibleException.java
@@ -1,7 +1,9 @@
 package com.ssafy.aitime.domain.exam.exception;
 
+import com.ssafy.aitime.domain.child.entity.enums.ChildHomeStatus;
+
 public class ExamNotEligibleException extends RuntimeException {
-    public ExamNotEligibleException(String message) {
-        super(message);
+    public ExamNotEligibleException() {
+        super("현재 검사를 시작할 수 없는 상태입니다. 현재 상태: %s");
     }
 }
