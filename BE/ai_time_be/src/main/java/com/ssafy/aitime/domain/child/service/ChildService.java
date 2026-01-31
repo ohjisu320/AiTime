585bbc1bbf1b7e57d9bfb5770ce91a239a504f42
soojin <wls7911@naver.com>
2026-01-31 21:41:53 +0900

    [BE-323] feat: 검사 진행도 조회 api 구현

--- BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/child/service/ChildService.java
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/child/service/ChildService.java
@@ -2,6 +2,7 @@ package com.ssafy.aitime.domain.child.service;
 
 import com.ssafy.aitime.domain.child.dto.request.ChildCreateRequest;
 import com.ssafy.aitime.domain.child.dto.request.ChildDeleteResponse;
+import com.ssafy.aitime.domain.child.dto.response.ChildAgeInfoResponse;
 import com.ssafy.aitime.domain.child.dto.response.ChildHomeResponse;
 import com.ssafy.aitime.domain.child.dto.response.ChildInfoResponse;
 import com.ssafy.aitime.domain.exam.dto.response.ExamStartResponse;
@@ -19,4 +20,5 @@ public interface ChildService {
     List<HospitalResponseDto> getLinkedHospitals(UUID userId, UUID childId);
 
     ExamStartResponse childStartExam(UUID userId, UUID childId, Boolean videoConsent);
+    ChildAgeInfoResponse validateAndGetChildAgeInfo(UUID userId, UUID childId);
 }
