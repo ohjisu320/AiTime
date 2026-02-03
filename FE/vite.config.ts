3004849227517289e113774c243c6d54b1253021
DESKTOP-6OO0P6F\SSAFY <dhwltn320@naver.com>
2026-02-03 21:18:01 +0900

    [FE-261] feat: 홈화면에 추가 버튼 생성

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
