e5ed7f37bbc7adf22ac1fac076d453296007f267
박윤영 <pyy2114@naver.com>
2026-02-04 09:02:54 +0900

    [BE-341] feat: 레포지토리 메서드 추가

--- BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/exam/repository/assessment/NameFacingEventRepository.java
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/exam/repository/assessment/NameFacingEventRepository.java
@@ -1,9 +1,12 @@
 package com.ssafy.aitime.domain.exam.repository.assessment;
 
 import com.ssafy.aitime.domain.exam.entity.assessment.NameFacingEvent;
+import com.ssafy.aitime.domain.exam.entity.assessment.NameFacingTrial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface NameFacingEventRepository extends JpaRepository<NameFacingEvent, UUID> {
+    List<NameFacingEvent> findByNameFacingTrial(NameFacingTrial trial);
 }
--- BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/exam/repository/assessment/PoseImitationEventRepository.java
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/exam/repository/assessment/PoseImitationEventRepository.java
@@ -1,9 +1,14 @@
 package com.ssafy.aitime.domain.exam.repository.assessment;
 
+import com.ssafy.aitime.domain.exam.entity.Video;
 import com.ssafy.aitime.domain.exam.entity.assessment.PoseImitationEvent;
+import com.ssafy.aitime.domain.exam.entity.assessment.PoseImitationTrial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PoseImitationEventRepository extends JpaRepository<PoseImitationEvent, UUID> {
+    List<PoseImitationEvent> findByPoseImitationTrial(PoseImitationTrial trial);
 }
