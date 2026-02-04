692e7403c10fcd095620af1bf53ea324f703fa3f
pyy2114 <pyy2114@naver.com>
2026-02-04 16:02:05 +0900

    [BE-341] feat: Video에 인덱스 추가

--- BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/exam/repository/VideoRepository.java
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/exam/repository/VideoRepository.java
@@ -2,8 +2,11 @@ package com.ssafy.aitime.domain.exam.repository;
 
 import com.ssafy.aitime.domain.exam.entity.Exam;
 import com.ssafy.aitime.domain.exam.entity.Video;
+import com.ssafy.aitime.domain.exam.entity.enums.AnalysisStatus;
 import com.ssafy.aitime.domain.exam.entity.enums.VideoType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
