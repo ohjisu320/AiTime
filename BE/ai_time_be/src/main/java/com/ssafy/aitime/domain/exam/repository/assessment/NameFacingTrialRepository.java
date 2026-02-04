400b2e47c5b3e913c688db23540e4a7ddb2abe90
박윤영 <pyy2114@naver.com>
2026-02-04 09:02:54 +0900

    [BE-341] feat: 레포지토리 메서드 추가

--- BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/exam/repository/assessment/NameFacingTrialRepository.java
+++ BE/ai_time_be/src/main/java/com/ssafy/aitime/domain/exam/repository/assessment/NameFacingTrialRepository.java
@@ -1,9 +1,12 @@
 package com.ssafy.aitime.domain.exam.repository.assessment;
 
+import com.ssafy.aitime.domain.exam.entity.Video;
 import com.ssafy.aitime.domain.exam.entity.assessment.NameFacingTrial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface NameFacingTrialRepository extends JpaRepository<NameFacingTrial, UUID> {
+    Optional<NameFacingTrial> findByVideo(Video video);
 }
