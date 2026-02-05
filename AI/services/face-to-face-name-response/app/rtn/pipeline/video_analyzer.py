b78e2b3694437f28633cec853f5fe1ffe694e0fb
박동한 <vkfkdtor00@gmail.com>
2026-02-05 10:21:58 +0900

    [AI-215] feat: openvino 적용

--- AI/services/face-to-face-name-response/app/rtn/pipeline/video_analyzer.py
+++ AI/services/face-to-face-name-response/app/rtn/pipeline/video_analyzer.py
@@ -25,6 +25,7 @@ from app.rtn.pipeline.window_analyzer import WindowAnalyzer
 from app.rtn.types import FrameBGR
 from app.rtn.vision.mp_face_detector import FaceDetectorMP
 from app.rtn.vision.mp_facemesh import FaceMeshMP
+from app.rtn.vision.openvino_face_detector import OpenVINOFaceDetector
 from app.rtn.vision.yolo_face_detector import YOLOFaceDetector
 
 logger = logging.getLogger("RTNAnalyzer.pipeline.video_analyzer")
@@ -52,7 +53,9 @@ class VideoAnalyzer:
         self.vad = SileroVAD(vad_cfg)
 
         # Detector selection
-        if face_cfg.model_selection == 1:
+        if face_cfg.model_selection == 2:
+            self.detector = OpenVINOFaceDetector(face_cfg.yolo_cfg)
+        elif face_cfg.model_selection == 1:
             self.detector = YOLOFaceDetector(face_cfg.yolo_cfg)
         else:
             self.detector = FaceDetectorMP(face_cfg)
